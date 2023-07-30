05202a102a2b013da11b18a32f160e7140af4f2f
RupertoTacuche <murcio2490@gmail.com>
2023-07-30 14:26:53 -0600

    labasedatosyafunciona

---
+++ src/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const app = require('./app');
+
+const { db } = require('./database/config');
+
+db.authenticate()
+  .then(() => console.log('Database authenticated ✌'))
+  .catch((err) => console.log(err));
+
+db.sync()
+  .then(() => console.log('Database Synced ✌'))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🙌`);
+});
